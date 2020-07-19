d24f57c6750355e2c690e688772756a36103ec91
mladjanupconfig <mladjan@upconfig.com>
2020-07-19 19:03:15 +0200

    create prototype header & footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,91 @@
 import React from 'react';
 import '../css/footer.css'
+import logo from "../images/logo.jpg";
 
 const Footer = () => {
     return (
         <footer className="footer">
-        FOOTER
+            <div className="footer-top">
+                <div className="wrapper">
+                    <div className="footer-logo">
+                        <img src={logo}></img>
+                    </div>
+                    <div className="footer-icons">
+                        <a href="https://www.facebook.com/pasazot/"> <i className="fa fa-facebook nf"></i></a>
+                        <a href="https://www.instagram.com/pasazot/?hl=sr"> <i className="fa fa-instagram ni"></i></a>
+                        <a href="https://twitter.com/pasazot?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"><i className="fa fa-twitter nt"></i></a>
+                    </div>
+                    <div className="footer-tittle">
+                        <h1>POSETITE NAS NA DRUSTVENIM MREZAMA</h1>
+                    </div>
+                    <div className="footer-text">
+                        <div className="footer-title">LEVIJATAN</div>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised</p>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised</p>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised</p>
+                    </div>
+                </div>
+            </div>
+            <div className="footer-bottom">
+                <div className="wrapper">
+                    <div className="footer-wrapper">
+                        <div className="footer-bottom-box-img">
+                            <ul>
+                                <li><img src={logo}></img></li>
+                                <li><a>Nesto</a></li>
+                                <li><a>Nesto</a></li>
+                                <li><a>Nesto</a></li>
+                            </ul>
+                        </div>
+                        <div className="footer-bottom-box">
+                            <ul>
+                                <li className="footer-box-title">Nesto</li>
+                                <li><a>Nesto</a></li>
+                                <li><a>Nesto</a></li>
+                                <li><a>Nesto</a></li>
+                                <li><a>Nesto</a></li>
+                            </ul>
+                        </div>
+                        <div className="footer-bottom-box">
+                            <ul>
+                                <li className="footer-box-title">Nesto</li>
+                                <li><a>Nesto</a></li>
+                                <li><a>Nesto</a></li>
+                                <li><a>Nesto</a></li>
+                                <li><a>Nesto</a></li>
+                            </ul>
+                        </div>
+                        <div className="footer-bottom-box">
+                            <ul>
+                                <li className="footer-box-title">Nesto</li>
+                                <li><a>Nesto</a></li>
+                                <li><a>Nesto</a></li>
+                                <li><a>Nesto</a></li>
+                                <li><a>Nesto</a></li>
+                            </ul>
+                        </div>
+                        <div className="footer-bottom-box">
+                            <ul>
+                                <li className="footer-box-title">Nesto</li>
+                                <li><a>Nesto</a></li>
+                                <li><a>Nesto</a></li>
+                                <li><a>Nesto</a></li>
+                                <li><a>Nesto</a></li>
+                            </ul>
+                        </div>
+                        <div className="footer-bottom-box">
+                            <ul>
+                                <li className="footer-box-title">Nesto</li>
+                                <li><a>Nesto</a></li>
+                                <li><a>Nesto</a></li>
+                                <li><a>Nesto</a></li>
+                                <li><a>Nesto</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
         </footer>
   
     );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,63 @@
 import '../css/header.css'
 
 import React, { Component } from 'react'
+import logo from "../images/logo.jpg";
 // import history from "../utils/history";
 
 export default class Header extends Component {
-
   componentDidMount() {
-    // const burger = document.querySelector('.burger');
-    // const closeNav = document.querySelector('.nav-close');
-    // const navList = document.querySelector('.nav-list');
-    // burger.addEventListener('click', function() {
-    //     navList.classList.add('show-nav');
-    // })
-
-    // closeNav.addEventListener('click', function() {
-    //     navList.classList.remove('show-nav');
-    // })
+    const burger = document.querySelector('.burger');
+    const mobileNav = document.querySelector('.nav-mobile-list');
+    burger.addEventListener("click", () => {
+      burger.classList.toggle("change");
+      mobileNav.classList.toggle("show-mobile-nav");
+    })
   }
 
   render() {
     return (
-      <div>
-        NAVIGATION
+      <div className="wrapper-nav">
+        <nav className="wrapper">
+          <div className="desktop-nav">
+            <div className="nav-left">
+              <ul>
+                <li><a>Pocetna</a></li>
+                <li><a>O nama</a></li>
+                <li><a>Video Arhiva</a></li>
+              </ul>
+            </div>
+            <div className="nav-logo">
+              <img src={logo}></img>
+            </div>
+            <div className="nav-right">
+              <ul>
+                <li><a>Clanska Karta</a></li>
+                <li><a>Prodavnica</a></li>
+                <li><a>Kontakt</a></li>
+              </ul>
+            </div>
+          </div>
+          <div className="mobile-nav">
+            <div className="nav-logo-mobile">
+              <img src={logo}></img>
+            </div>
+            <div className="burger">
+              <div className="bar1"></div>
+              <div className="bar2"></div>
+              <div className="bar3"></div>
+            </div>
+            <div className="nav-mobile-list">
+              <ul>
+                <li><a>Pocetna</a></li>
+                <li><a>O nama</a></li>
+                <li><a>Video Arhiva</a></li>
+                <li><a>Clanska Karta</a></li>
+                <li><a>Prodavnica</a></li>
+                <li><a>Kontakt</a></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
       </div>
     )
   }
