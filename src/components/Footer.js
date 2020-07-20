aa6aa344e11bb81d1cb1112f2abe20ad0e544623
mladjanupconfig <mladjan@upconfig.com>
2020-07-20 21:52:55 +0200

    add banner

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../css/footer.css'
+import '../css/footer.css';
 import logo from "../images/logo.png";
 import logoNoText from "../images/logo-without-text.png";
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,19 @@
 import React, { Component} from 'react';
-
+import bannerImg from "../images/slikapsa.png";
+import bannerText from "../images/ukljuciseipodrzinas.png";
+import '../css/home.css'
 export default class Home extends Component {
 
     render() {
         
         return (
             <div>
-                   HOME 
+                <div className="banner-wrapper">
+                    <div className="banner-text-wrapper">
+                        <img className="banner-text" src={bannerText}></img>
+                    </div>
+                    <img src={bannerImg}></img>
+                </div>
             </div>
         )
     }
