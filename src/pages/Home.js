483b3512fce354ccf5df56255abc3fe6d415ffe5
mladjanupconfig <mladjan@upconfig.com>
2020-07-20 22:09:47 +0200

    add animation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React, { Component} from 'react';
 import bannerImg from "../images/slikapsa.png";
 import bannerText from "../images/ukljuciseipodrzinas.png";
 import '../css/home.css'
+import { Transition } from 'react-spring/renderprops'
 export default class Home extends Component {
 
     render() {
@@ -9,10 +10,21 @@ export default class Home extends Component {
         return (
             <div>
                 <div className="banner-wrapper">
-                    <div className="banner-text-wrapper">
-                        <img alt="bannerText" className="banner-text" src={bannerText}></img>
-                    </div>
-                    <img alt="bannerImg" src={bannerImg}></img>
+                    <Transition
+                        items={<div className="banner-text-wrapper"><img alt="bannerText" className="banner-text" src={bannerText}></img></div>} keys={item => item}
+                        from={{ opacity: 0, visibility: 'hidden', transition: 'all 1.5s ease-in-out' }}
+                        enter={{ opacity: 1, visibility: 'visible' }}
+                        leave={{ opacity: 1, visibility: 'visible' }}>
+                        {item => props => <div style={props}>{item}</div>}
+                    </Transition>
+                    <Transition
+                        items={<img alt="bannerImg" src={bannerImg}></img>} keys={item => item}
+                        from={{ transform: 'translateX(60%)', opacity: 0, visibility: 'hidden', transition: 'all 1.5s ease-in-out' }}
+                        enter={{ transform: 'translateX(0)', opacity: 1, visibility: 'visible' }}
+                        leave={{ transform: 'translateX(0)', opacity: 1, visibility: 'visible' }}>
+                        {item => props => <div style={props}>{item}</div>}
+                    </Transition>
+                    
                 </div>
             </div>
         )
