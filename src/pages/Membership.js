24da7741688c5aa93cf3a7b3c970a6f450d75367
mladjanupconfig <mladjan@upconfig.com>
2020-07-21 18:38:56 +0200

    add membership page

--- src/pages/Membership.js
+++ src/pages/Membership.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react'
-
+import cards from '../json/cards.json';
+import "../css/membership.css";
 export default class Membership extends Component {
     render() {
         return (
-            <div>
-                Membership
+            <div className="membership-page-wrapper">
+                <div className="wrapper">
+                <div className="title"><h1>Membership</h1></div>
+                <div className="content-wrapper">
+                    <p>Hvala Vam što podržavate Levijatan.</p>
+                    <div className="cards">
+                        {cards.map(card => {
+                            return <div key={card.desc} className="card">
+                                <div className="card-image"><img alt={card.alt} src={card.img}></img></div>
+                                <p>{card.desc}</p>
+                            </div>
+                        })}
+                    </div>
+                    <p className="desc-payment">Uplata se vrši unapred, putem našeg dinarskog ili paypal naloga, a onda uz poslate podatke krećemo sa izradom kartica.</p>
+                </div>
+                
+                </div>
+                
             </div>
         )
     }
