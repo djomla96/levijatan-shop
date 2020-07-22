f0497e9dd9788f2fc555f65636e3e33bb0a3a320
mladjanupconfig <mladjan@upconfig.com>
2020-07-22 18:48:05 +0200

    add shop page prototip

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
+import "../css/shop.css";
 
 export default class Shop extends Component {
     render() {
         return (
-            <div>
-                SHOP
+            <div className="shop">
+            <div className="wrapper">
+                <h1>Prodavnica</h1>
+                <p>Ovde ce biti shop stranica</p>
             </div>
+        </div>
         )
     }
 }
